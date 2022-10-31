b64e53e91b9852b56b77384d7cf2965ee12d3385
VarshithaSane <70535434+VarshithaSane@users.noreply.github.com>
2022-10-31 16:54:06 +0530

    Update helloWorldExternal.groovy

--- vars/helloWorldExternal.groovy
+++ vars/helloWorldExternal.groovy
@@ -1,4 +1,4 @@
 def call(Map config = [:]) {
   loadLinuxScript(name : 'hello-world.sh')
-  sh "./hello-world.sh ${config.name} ${config.dayofweek}
+  sh "./hello-world.sh ${config.name} ${config.dayofweek}"
 }
