29f7af733ae9a5e1f41fc10c87321c5a25197fbe
VarshithaSane <70535434+VarshithaSane@users.noreply.github.com>
2022-10-31 14:04:28 +0530

    Update helloWorld.groovy

--- vars/helloWorld.groovy
+++ vars/helloWorld.groovy
@@ -2,6 +2,13 @@
 //     sh "echo Hello ${name}. Today is ${dayofweek}."
 // }
 
-def call(Map config = [:]) {
-    sh "echo Hello ${config.name}. Today is ${config.dayofweek}."
+// def call(Map config = [:]) {
+//     sh "echo Hello ${config.name}. Today is ${config.dayofweek}."
+// }
+
+class Example {
+   static void main(String[] args) {
+      // Using a simple println statement to print output to the console
+      println('Hello World');
+   }
 }
