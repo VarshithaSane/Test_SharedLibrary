df6e9bb2f52a54ac68160ffcb44b3e180a4fb811
VarshithaSane <70535434+VarshithaSane@users.noreply.github.com>
2022-11-03 16:52:52 +0530

    Update test.groovy

--- vars/test.groovy
+++ vars/test.groovy
@@ -1,3 +1,28 @@
 def call() {
-    sh "echo testing branching."
+    sh "echo testing"
 }
+
+
+
+// def call(String name, String dayofweek) {
+    
+//     println "Hello ${name}. Today is ${dayofweek}."
+//     sh "echo Hello ${name}. Today is ${dayofweek}."
+// }
+
+// def call(Map config = [:]) {
+//     sh "echo Hello ${config.name}. Today is ${config.dayofweek}."
+// }
+
+// class Example {
+//    static void main(String[] args) {
+//       // Using a simple println statement to print output to the console
+//       println('Hello World');
+//    }
+// }
+
+// class Simple{  
+//     public static void main(String args[]){  
+//      System.out.println("Hello Java");  
+//     }  
+// } 
