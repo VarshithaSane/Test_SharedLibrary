710f29393341a0cc3dd98edd040883e39531b17f
VarshithaSane <70535434+VarshithaSane@users.noreply.github.com>
2022-11-10 13:52:56 +0530

    Update helloWorld.groovy

--- vars/helloWorld.groovy
+++ vars/helloWorld.groovy
@@ -1,5 +1,5 @@
-def call(String name, String dayofweek) {
-    sh "echo Hello ${name}. Today is ${dayofweek}"
+def call(Map config = [:]) {
+sh "echo Hello ${config.name}. Today is ${config.dayofweek}."
 }
 
 
